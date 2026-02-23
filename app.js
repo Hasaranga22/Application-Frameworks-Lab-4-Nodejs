529e4006896b8270571836faa1e98b276632c108
Navoda Hasaranga <hasaranganavoda2025@gmail.com>
2026-02-23 21:26:51 +0530

    Remove file reading code from app.js
    Removed file reading functionality from app.js.

--- app.js
+++ app.js
@@ -2,12 +2,6 @@ console.log("Lab 04 - Node JS Working!");
 
 const fs = require('fs');
 
-// Read from a file
-fs.readFile('file.txt', 'utf8', function (err, data) {
-    if (err) throw err;
-    console.log(data);
-});
-
 // Write to a file
 fs.writeFile('file.txt', 'Hello World!', function (err) {
     if (err) throw err;
