9d96b2df258f08d6f4647b9203ee92674617d82e
Navoda Hasaranga <hasaranganavoda2025@gmail.com>
2026-02-23 21:22:29 +0530

    Added web server example

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-console.log("Lab 04 - Node JS Working!");
+const http = require('http');
 
-const fs = require('fs');
-
-fs.readFile('file.txt', 'utf8', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
